f1dcd9af9b0d4c7b080053261ba2fd0dd235d00a
Marcio Junior <marcio.mj70@gmail.com>
2021-10-02 12:23:41 -0300

    feat: auth context

---
+++ client/src/contexts/AuthContext.tsx
@@ -0,0 +1,56 @@
+import { createContext, useState, useEffect, ReactNode } from 'react';
+import api from "../services/api"
+import history from "../services/history"
+
+type AuthContextType = {
+  loading: boolean;
+  authenticated: boolean;
+  handleLogin: ({ email, password }: UserRequest) => Promise<void>
+}
+
+type AuthProviderProps = {
+  children: ReactNode;
+}
+
+type UserRequest = {
+  email: string;
+  password: string;
+}
+
+const AuthContext = createContext({} as AuthContextType);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [authenticated, setAuthenticated] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      api.defaults.headers.Authorization = `Bearer ${token}`;
+      setAuthenticated(true)
+    }
+
+    setLoading(false)
+  }, [])
+
+  async function handleLogin({ email, password }: UserRequest) {
+    const { data } = await api.post('/login', { email, password })
+
+    localStorage.setItem('token', data)
+    api.defaults.headers.Authorization = `Bearer ${data}`;
+    setAuthenticated(true)
+
+    setTimeout(() => {
+      history.push('/videos')
+    }, 2000)
+  }
+
+  return (
+    <AuthContext.Provider value={{ loading, authenticated, handleLogin }}>
+      {children}
+    </ AuthContext.Provider>
+  )
+}
+
+export { AuthContext, AuthProvider }

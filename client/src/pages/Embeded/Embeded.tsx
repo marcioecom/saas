f9a1f4a2e0973787eb156db7440fd07da009023c
Marcio Junior <marcio.mj70@gmail.com>
2021-10-25 14:05:10 -0300

    feat: embeded video v0.1

--- client/src/pages/Embeded/Embeded.tsx
+++ client/src/pages/Embeded/Embeded.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import { useParams } from 'react-router';
+
+import "./embeded.css"
+interface IEmbededParams {
+  videoId: string;
+}
 
 const Embeded = () => {
   // const [videoUrl, setVideoUrl] = useState('#')
   const baseUrl = process.env.REACT_APP_APP_URL
+  const { videoId } = useParams<IEmbededParams>()
 
+  const videoSource = `${baseUrl}/stream/${videoId}`
   return (
     <>
-      <video
-        width="600px"
-        controls
-        autoPlay
-      >
-        <source src={`${baseUrl}/stream`} type="video/mp4" />
-      </video>
+      <div className="video-wrapper">
+        <video
+          src={videoSource}
+          height="100%"
+          width="100%"
+          controls
+        />
+        <div className="video-controls">
+          
+        </div>
+      </div>
     </>
   );
 }

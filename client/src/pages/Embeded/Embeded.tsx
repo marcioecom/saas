e0ae6ced06af2ef6acac9726ce414f0907dcc92b
Marcio Junior <marcio.mj70@gmail.com>
2021-12-30 01:14:23 -0300

    fix(build): add cdn to css player file

--- client/src/pages/Embeded/Embeded.tsx
+++ client/src/pages/Embeded/Embeded.tsx
@@ -1,39 +1,35 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router';
-import Plyr from 'plyr';
-import 'plyr/dist/plyr.css'
+import React, { useRef } from 'react';
+// import { useParams } from 'react-router';
+import Plyr, { APITypes } from 'plyr-react';
 
-interface IEmbededParams {
-  videoId: string;
-}
+// interface IEmbededParams {
+//   videoId: string;
+// }
 
 const Embeded = () => {
-  const { videoId } = useParams<IEmbededParams>()
-  const baseUrl = process.env.REACT_APP_APP_URL
+  const ref = useRef<APITypes>(null)
+  // const { videoId } = useParams<IEmbededParams>()
+  // const baseUrl = process.env.REACT_APP_APP_URL
   // const videoSource = `${baseUrl}/stream/${videoId}`
   const videoSource = "https://vclick-raw-videos.s3.us-east-2.amazonaws.com/17c8a8acc751a1792d100a9df905dc51-video.mp4"
-
-  useEffect(() => {
-    new Plyr('#player', { 
-      controls: [
-        'play-large',
-        'rewind',
-        'play',
-        'volume',
-        'fullscreen'
-      ] 
-    });
-  }, [])
 
   return (
     <>
       <div className="video-wrapper">
-        <video
-          src={videoSource}
-          height="100%"
-          width="100%"
-          controls
-          id="player"
+        <Plyr
+          ref={ref}
+          source={{
+            type: "video",
+            sources: [{ src: videoSource }]
+          }}
+          options={{
+            controls: [
+              'play-large',
+              'rewind',
+              'play',
+              'volume',
+              'fullscreen'
+            ]}}
         />
       </div>
     </>

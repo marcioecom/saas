724ec138bf0133cfaf310b789a450d9de465580b
Marcio Junior <marcio.mj70@gmail.com>
2021-12-30 00:27:09 -0300

    feat: video player Plyr

--- client/src/pages/Embeded/Embeded.tsx
+++ client/src/pages/Embeded/Embeded.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
+import Plyr from 'plyr';
+import 'plyr/dist/plyr.css'
 
-import "./embeded.css"
 interface IEmbededParams {
   videoId: string;
 }
 
 const Embeded = () => {
-  const baseUrl = process.env.REACT_APP_APP_URL
   const { videoId } = useParams<IEmbededParams>()
-  
-  const videoSource = `${baseUrl}/stream/${videoId}`
+  const baseUrl = process.env.REACT_APP_APP_URL
+  // const videoSource = `${baseUrl}/stream/${videoId}`
+  const videoSource = "https://vclick-raw-videos.s3.us-east-2.amazonaws.com/17c8a8acc751a1792d100a9df905dc51-video.mp4"
+
+  useEffect(() => {
+    new Plyr('#player', { 
+      controls: [
+        'play-large',
+        'rewind',
+        'play',
+        'volume',
+        'fullscreen'
+      ] 
+    });
+  }, [])
+
   return (
     <>
       <div className="video-wrapper">
@@ -19,10 +33,8 @@ const Embeded = () => {
           height="100%"
           width="100%"
           controls
+          id="player"
         />
-        <div className="video-controls">
-          
-        </div>
       </div>
     </>
   );

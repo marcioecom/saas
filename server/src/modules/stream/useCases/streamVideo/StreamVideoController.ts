625c8fa62e092f5f8315444159ffa0090a231102
Marcio Junior <marcio.mj70@gmail.com>
2022-01-03 23:17:21 -0300

    refactor: stream video sends video info

--- server/src/modules/stream/useCases/streamVideo/StreamVideoController.ts
+++ server/src/modules/stream/useCases/streamVideo/StreamVideoController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { S3 } from "aws-sdk";
 import { StreamVideo } from "./StreamVideo";
 
 class StreamVideoController {
@@ -9,34 +8,14 @@ class StreamVideoController {
   async handle(req: Request, res: Response) {
     const { id } = req.params;
 
-    const s3 = new S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    });
-
     try {
       const video = await this.streamVideo.execute(id);
 
-      const getParams = {
-        Bucket: "vclick-raw-videos",
-        Key: video.key,
-      };
-
-      const s3Stream = s3
-        .getObject(getParams)
-        .createReadStream() // { highWaterMark: 1 * 1024 }
-        .pipe(res);
-
-      s3Stream.on("error", err => {
-        console.error(err);
+      return res.json(video);
+    } catch (error: any) {
+      return res.json({
+        message: error.message,
       });
-      s3Stream.on("close", () => {
-        console.log("Done.");
-      });
-
-      return s3Stream;
-    } catch (error) {
-      return res.json(error);
     }
   }
 }
